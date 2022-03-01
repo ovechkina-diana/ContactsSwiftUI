ebfc8be8bd7f61d40bafe09db41d3e21bc6f26d6
Diana Ovechkina <ovechkinadiana@yandex.ru>
2022-03-01 21:53:23 +0300

    Initial Commit

---
+++ ContactsSwiftUI/ContactsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsSwiftUIApp.swift
+//  ContactsSwiftUI
+//
+//  Created by Diana Ovechkina on 01.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
